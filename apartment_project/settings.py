644e1a95bb915a7ee4ad9a0ef1392593794fbe38
travisspears0 <travisspears0@gmail.com>
2017-09-21 23:05:04 +0200

    auth disabled

--- apartment_project/settings.py
+++ apartment_project/settings.py
@@ -73,9 +73,8 @@ WSGI_APPLICATION = 'apartment_project.wsgi.application'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
     'PAGE_SIZE': 10
 }
 
